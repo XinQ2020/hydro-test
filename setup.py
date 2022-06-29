04b4a495256640f220cc9e847387e5de0d7956b2
“XinQ2020” <1969848456@qq.com>
2022-06-29 11:40:23 +0800

    first commit by XQ

--- setup.py
+++ setup.py
@@ -13,20 +13,20 @@ Copyright (c) 2021-2022 Wenyu Ouyang. All rights reserved.
 from setuptools import setup, find_packages
 
 setup(
-    name = "hydrotest", # 输入项目名称
-    version = "0.0.1", # 输入版本号
-    keywords = ["test"], # 输入关键词
-    description = "hydro test", # 输入概述
-    long_description = "a test repo for pypi packaging", # 输入描述
+    name = "hydrotest", 
+    version = "0.0.1", 
+    keywords = ["test"], 
+    description = "hydro test", 
+    long_description = "a test repo for pypi packaging", 
 
-    url = "https://github.com/OuyangWenyu/hydro-test", # 输入项目Github仓库的链接
-    author = "Wenyu Ouyang", # 输入作者名字
-    author_email = "wenyuouyang@outlook.com", # 输入作者邮箱
-    license= "MIT_license", # 此为声明文件，一般填写 MIT_license
+    url = "https://github.com/OuyangWenyu/hydro-test",
+    author = "Wenyu Ouyang", 
+    author_email = "wenyuouyang@outlook.com",
+    license= "MIT_license", 
 
     packages = find_packages(),
     include_package_data = True,
     platforms = "any",
-    install_requires = ["numpy"], # 输入项目所用的包
-    python_requires='>=3.9', # Python版本要求
+    install_requires = ["numpy"], 
+    python_requires='>=3.9', 
 )
